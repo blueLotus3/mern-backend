ab092ecbd7ffa542d185a29eb13a6f39f7164314
Troy Lemons Jr <troylemonsjr@gmail.com>
2021-02-02 16:28:39 -0800

    fixing it up

--- server.js
+++ server.js
@@ -1,25 +1,35 @@
-require("dotenv").config()
-const cors = require("cors")
-const corsOptions = require("./configs/cors.js")
+require('dotenv').config()
+const { PORT = 4000, NODE_ENV = "development" } = process.env;
 const express = require('express')
 const logger = require('morgan')
 const app = express()
 
-const {PORT = 3000, NODE_ENV = "development"} = process.env
-
-NODE_ENV === "production" ? app.use(cors(corsOptions)) : app.use(cors());
-app.use(logger('dev'))
+// Add the middleware code needed to accept incoming data and add it to req.body
+app.use(logger('dev'));
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
-app.use(logger('tiny'));
-app.use(cors(corsOptions))
+
+//CORS
+const cors = require("cors");
+const corsOptions = require("./configs/cors.js");
+
+////////////
+//MIDDLEWARE
+////////////
+NODE_ENV === "production" ? app.use(cors(corsOptions)) : app.use(cors());
 
 
-const gloveRouter = require('./controllers/glove')
-app.use('/gloves/', gloveRouter)
+//Route for testing server is working
+app.get("/", (req, res) => {
+    res.json({ hello: "Hello World!" });
+  });
 
-const boxerRouter = require('./controllers/boxer')
-app.use('/boxers/', boxerRouter)
+
+// const gloveRouter = require('./controllers/glove')
+// app.use('/gloves/', gloveRouter)
+
+// const boxerRouter = require('./controllers/boxer')
+// app.use('/boxers/', boxerRouter)
 
 
 app.listen(PORT, () => {
